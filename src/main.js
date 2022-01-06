1abffcaedcb05d4fe2b75e5ee8848621dd986f14
王璐 <568818216@qq.com>
2022-01-06 18:17:15 +0800

    首页菜单雏形

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import SlideVerify from 'vue-monoplasty-slide-verify';
 import VueParticles from 'vue-particles'
-import axios from 'axios'
+import store from 'store'
 
 import{
 		getRequest,
 		putRequest,
 		deleteRequest,
 		postRequest
-	}from './utils/api.js'
+	}from './utils/api'
 Vue.use(VueParticles)
 Vue.use(ElementUI)
 Vue.use(SlideVerify)
@@ -27,5 +27,6 @@ Vue.prototype.deleteRequest = deleteRequest;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/Login.vue'
-import Home from '../views/Home.vue'
+import Login from '../views/Login'
+import Home from '../views/Home'
+import test1 from '../views/test1'
+import test2 from '../views/test2'
 
 Vue.use(VueRouter)
 
@@ -9,17 +11,33 @@ const routes = [
   {
     path: '/',
     redirect: "/login",
+	hidden:true
   },
   {
     path: '/login',
     name: '登录',
-    component: Login
+    component: Login,
+	hidden:true
   },
   {
     path: '/home',
     name: '主页',
-    component: Home
-  }
+    component: Home,
+	children:[
+		{
+		  path: '/test1',
+		  name: 'test1',
+		  component: test1
+		},
+		{
+		  path: '/test2',
+		  name: 'test2',
+		  component: test2
+		}
+	]
+	
+  },
+
 ]
 
 const router = new VueRouter({
