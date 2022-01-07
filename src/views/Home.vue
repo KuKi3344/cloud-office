f5b5a8c4b5c27c36c7e36f36c3a316d49a63cc3a
王璐 <568818216@qq.com>
2022-01-07 12:57:10 +0800

    从vuex中获取初始化（请求菜单）路由时，存储到vuex的路由数据，并渲染到页面导航上

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,24 @@
 		<el-container>
 			<el-header>header</el-header>
 			<el-container>
-				<el-aside width="200px">
-					    <el-menu router>
-					      <el-submenu index="1" v-for="(item,index) in this.$router.options.routes" 
-						  :key="index" v-if="!item.hidden">
+				<el-aside width="200px" style="background-color:#3e454c;opacity: 0.8;border-radius:10px;
+    border: 1px solid hsla(0,0%,100%,.8);box-shadow: 10px 10px 20px rgb(0 0 0 / 40%);">
+					    <el-menu router style="width:100% !important">
+					      <el-submenu :index="index+''" v-for="(item,index) in routes" 
+						  :key="index" v-if="!item.hidden" style="background-color:#3e454c;">
 					        <template slot="title">
-								<i class="el-icon-location"></i>
+						<!-- 		需要下载font-awesome才能用这个图标 -->
+								<i :class="item.iconCls" style="color:#adc9e3;margin-right:15px;"></i>
 							<span>{{item.name}}</span>
 							</template>
-					        <el-menu-item :index="children.path" v-for="(children,indexj) in item.children">
-							  {{children.name}}
+					    <el-menu-item :index="subMenu.path" v-for="(subMenu,indexj) in item.subMenu" :key="indexj">
+							  {{subMenu.name}}
 							  </el-menu-item>
 							</el-submenu>
 							 </el-menu>
 				</el-aside>
 				<el-main>
-					<router-view />
+					  <router-view/>
 				</el-main>
 			</el-container>
 		</el-container>
@@ -28,6 +30,12 @@
 <script>
 	export default{
 		name:"Home",
+		computed:{
+			routes(){
+				// console.log(this.$store.state.routes);
+				return this.$store.state.routes;
+			}
+		},
 		methods:{
 			
 		}
@@ -35,5 +43,6 @@
 	}
 </script>
 
-<style>
+<style scoped>
+	@import url(../assets/css/home.css);
 </style>
